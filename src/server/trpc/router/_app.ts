eea298b4466a01d4623a053990b91ed195ea0ca2
Jonathan Aires <airesjonathan123@gmail.com>
2023-04-21 10:08:02 -0300

    [feat]Deletar-rota-examplets

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,14 +1,12 @@
 import { router } from "../trpc";
 import { adminRouter } from "./admin";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { lessonRouter } from "./lesson";
 import { moduleRouter } from "./modules";
 import { taskRouter } from "./task";
 import { userRouter } from "./user";
 
 export const appRouter = router({
-  example: exampleRouter,
   module: moduleRouter,
   lesson: lessonRouter,
   auth: authRouter,
