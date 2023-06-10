12a50478a4bdc61fd0bd6b1821ffd3d1db82d0eb
Lu1z-Gust4v0 <Lu1z-Gust4v0@users.noreply.github.com>
2023-06-10 21:41:50 +0000

    Apply formatting changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@
 export const PORT = process.env.PORT ?? 3000;
 export const LOCAL_HOST = `http://localhost:${PORT}`;
 export const VERCEL_URL = process.env.VERCEL_URL;
-export const IN_BROWSER = typeof window !== "undefined"; 
+export const IN_BROWSER = typeof window !== "undefined";
 export const IN_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 export enum Roles {
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -3,15 +3,20 @@ import { createTRPCNext } from "@trpc/next";
 import { type GetInferenceHelpers } from "@trpc/server";
 import superjson from "superjson";
 import { type AppRouter } from "../server/trpc/router/_app";
-import { LOCAL_HOST, VERCEL_URL, IN_BROWSER, IN_DEVELOPMENT } from "./constants";
+import {
+  LOCAL_HOST,
+  VERCEL_URL,
+  IN_BROWSER,
+  IN_DEVELOPMENT,
+} from "./constants";
 
 const getBaseUrl = () => {
   // browser should use relative url
-  if (IN_BROWSER) return ""; 
+  if (IN_BROWSER) return "";
   // SSR should use vercel url
-  if (VERCEL_URL) return VERCEL_URL; 
+  if (VERCEL_URL) return VERCEL_URL;
   // dev SSR should use localhost
-  return LOCAL_HOST; 
+  return LOCAL_HOST;
 };
 
 export const trpc = createTRPCNext<AppRouter>({
@@ -21,7 +26,8 @@ export const trpc = createTRPCNext<AppRouter>({
       links: [
         loggerLink({
           enabled: (opts) =>
-            IN_DEVELOPMENT || (opts.direction === "down" && opts.result instanceof Error),
+            IN_DEVELOPMENT ||
+            (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
